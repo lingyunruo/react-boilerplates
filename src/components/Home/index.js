5e162091cb771cf2fbd51d0065acace006abce26
lingyun01 <lingyun01@58ganji.com>
2017-09-01 15:32:33 +0800

    react的模版文件

---
+++ src/components/Home/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import './index.scss';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>这是一个测试页面</div>
+        )
+    }
+}
+
+export default Home;
